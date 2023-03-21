3cccb4244bb31db8507b65a8803a978559c9e751
kajela <titoyut56@gmail.com>
2023-03-21 10:53:31 -0400

    add the initial files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -50,5 +50,89 @@ public class App {
             res.redirect("/");
             return null;
         }, new HandlebarsTemplateEngine());
+        get("/courses/delete", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            courseDao.clearAllCourses();
+            studentDao.clearAll();
+            res.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
+//        //get: delete all infections
+
+        get("/students/delete", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            studentDao.clearAll();
+            res.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
+        get("/courses/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfCourseToFind = Integer.parseInt(req.params("id")); //new
+            Course foundCourse = courseDao.findById(idOfCourseToFind);
+            model.put("course", foundCourse);
+            List<Student> allStudentByCourses = courseDao.getAllStudentsByCourse(idOfCourseToFind);
+            model.put("students", allStudentByCourses);
+            model.put("courses", courseDao.getAll()); //refresh list of links for navbar
+            return new ModelAndView(model, "course-detail.hbs"); //new
+        }, new HandlebarsTemplateEngine());
+
+//        //get: show a form to update a category
+
+        get("/courses/:id/edit", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("editCourse", true);
+            Course course = courseDao.findById(Integer.parseInt(req.params("id")));
+            model.put("course", course);
+            model.put("courses", courseDao.getAll()); //refresh list of links for navbar
+            return new ModelAndView(model, "course-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/courses/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfCourseToEdit = Integer.parseInt(req.params("id"));
+            String newName = req.queryParams("newCourseName");
+            courseDao.update(idOfCourseToEdit, newName);
+            res.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
+//        //get: delete an individual viral test
+
+        get("/courses/:course_id/students/:student_id/delete", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfStudentToDelete = Integer.parseInt(req.params("student_id"));
+            studentDao.deleteById(idOfStudentToDelete);
+            res.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
+        get("/students/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Course> courses = courseDao.getAll();
+            model.put("courses", courses);
+            return new ModelAndView(model, "student-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+//        //task: process new viral form
+        post("/students", (req, res) -> { //URL to make new task on POST route
+            Map<String, Object> model = new HashMap<>();
+            List<Course> allCourses = courseDao.getAll();
+            model.put("courses", allCourses);
+            String name = req.queryParams("name");
+            String reg = req.queryParams("reg");
+            String email = req.queryParams("email");
+            String tel = req.queryParams("tel");
+            String unit = req.queryParams("unit");
+            String lecture = req.queryParams("lecture");
+            String dateTaught = req.queryParams("dateTaught");
+            String studentRemark = req.queryParams("studentRemark");
+            int courseId = Integer.parseInt(req.queryParams("courseId"));
+            Student newStudent = new Student(name,reg,email,tel,unit,lecture,dateTaught,studentRemark,courseId);        //See what we did with the hard coded categoryId?
+            studentDao.add(newStudent);
+            res.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
     }
 }
