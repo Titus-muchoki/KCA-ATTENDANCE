5868ad1447af04d32366f359b489992ac4d075bf
kajela <titoyut56@gmail.com>
2023-03-23 13:58:24 -0400

    add the initial files

--- src/main/java/models/Course.java
+++ src/main/java/models/Course.java
@@ -22,19 +22,15 @@ public class Course {
     public int hashCode() {
         return Objects.hash(name, id);
     }
-
     public int getId() {
         return id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public void setId(int id) {
         this.id = id;
     }
